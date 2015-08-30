0fb2a2f090fbb2bc2e099f8f5940d7c028dcd9e6
Egor <sapronov.egor@gmail.com>
2015-08-30 16:51:41 +0600

    style(navbar): mdl navbar

--- app.js
+++ app.js
@@ -3,8 +3,12 @@
 let express = require('express');
 let app = express();
 
+app.use('/static', express.static('./web/dist'));
+app.set('view engine', 'jade');
+app.set('views', './client/templates');
+
 app.get('/', function (req, res) {
-	res.send('Hello');
+	res.render('index');
 });
 
 module.exports = app;
