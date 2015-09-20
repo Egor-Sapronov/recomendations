76015ee55d12060b96b79af9459188c1f5df52a6
Egor <sapronov.egor@gmail.com>
2015-09-20 17:00:16 +0600

    feat(db connection): connect to mongodb by url from env

---
+++ libs/database/mongoose.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on('error', err => {
+	console.log(err);
+});
+
+db.on('open', () => {
+	console.log('connected to db');
+});
