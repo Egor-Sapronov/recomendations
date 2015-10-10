8ba2b4dab29aacea8cd1048a1e70a3e33fc51ea9
Егор <egor@MacBook-Pro-mac.local>
2015-10-10 16:00:46 +0600

    update

--- router/resources/recomendations.js
+++ router/resources/recomendations.js
@@ -1,5 +1,26 @@
 'use strict';
 
 const router = require('express').Router();
+const db = require('../../libs/database/mongoose');
+
+router.get('/recomendations', (req, res) => {
+
+});
+
+router.get('/recomendations/:id', (req, res) => {
+
+});
+
+router.post('/recomendations', (req, res) => {
+
+});
+
+router.put('/recomendations/:id', (req, res) => {
+
+});
+
+router.delete('/recomendations/:id', (req, res) => {
+
+});
 
 module.exports = router;
