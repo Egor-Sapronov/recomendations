72c27a2ed993d264189ac2a7113a20a72c50a518
Егор <egor@MacBook-Pro-mac.local>
2015-11-02 22:45:02 +0600

    fix

--- client/app/components/embed.js
+++ client/app/components/embed.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default props => (
     <div>
         <a target="_blank" href={props.url}><h4>{props.title}</h4></a>
-        {props.thumbnail_url ? <img style={{maxWidth:'100%', height: 'auto'}} src={props.thumbnail_url} /> : '' }
+        {(props.thumbnail_url && !props.html) ? <img style={{maxWidth:'100%', height: 'auto'}} src={props.thumbnail_url} /> : '' }
         {props.html ? <div dangerouslySetInnerHTML={{ __html: props.html }} /> : ''}
         <p>{props.description}</p>
     </div>
