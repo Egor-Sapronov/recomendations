5a845012c9813f376e3ea262ed8cd3469c98582f
Егор <egor@MacBook-Pro-mac.local>
2015-11-06 23:22:10 +0600

    fix

--- libs/database/schemas/user.js
+++ libs/database/schemas/user.js
@@ -1,26 +1,25 @@
 const Schema = require('mongoose').Schema;
 const User = new Schema({
-  providerId: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  provider: {
-    type: String,
-  },
-  profileLink: {
-    type: String,
-    unique: true,
-  },
-  displayName: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
+    providerId: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    provider: {
+        type: String,
+    },
+    profileLink: {
+        type: String,
+    },
+    displayName: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
 });
 
 module.exports = User;
