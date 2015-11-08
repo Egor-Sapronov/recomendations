593f8f4ca11240fb0a60dad0e6daf390de22f9b3
Егор <sapronov.egor@gmail.com>
2015-11-08 19:49:02 +0600

    update

--- client/app/components/login.js
+++ client/app/components/login.js
@@ -4,7 +4,9 @@ export default () => {
     return (
         <div className="mdl-grid" style={{padding: '80px'}}>
             <div className="mdl-layout-spacer" />
-            <a href="/facebook">signin</a>
+            <a href='/facebook' className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored">
+                signin
+            </a>
             <div className="mdl-layout-spacer" />
         </div>
     );
