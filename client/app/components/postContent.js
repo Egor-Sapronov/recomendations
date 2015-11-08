76a2c19be7555540dcd775e959727c71979fe15d
Егор <sapronov.egor@gmail.com>
2015-11-08 19:25:32 +0600

    fix

--- client/app/components/postContent.js
+++ client/app/components/postContent.js
@@ -3,7 +3,7 @@ import Embed from './embed';
 
 export default (props) => {
     return (
-        <div className="mdl-card__supporting-text">
+        <div className="mdl-card__supporting-text" style={{ width: null }}>
             {props.data.map(item => <Embed key={ item._id } {...item} />)}
             {props.linkedContent ? <div dangerouslySetInnerHTML={{ __html: props.linkedContent}} /> : '' }
         </div>
