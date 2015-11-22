acbdab89e8a4111a13a2d40b305c0a818b0e8c22
Egor <sapronov.egor@gmail.com>
2015-11-22 15:29:18 +0600

    update

--- client/app/components/facebookButton.js
+++ client/app/components/facebookButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
     return (
-      <a href="/facebook" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored">
+      <a href="/facebook" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored mdl-typography--text-lowercase">
           <i className="icon-facebook2" style={{marginRight: '10px'}} />
           log in with facebook
       </a>
--- client/app/components/login.js
+++ client/app/components/login.js
@@ -5,17 +5,15 @@ import FacebookButton from './facebookButton';
 export default () => {
     return (
         <div>
-            <div className="mdl-grid" style={{paddingTop: '60px'}}>
-                <div className="mdl-layout-spacer" />
-                <p className="mdl-typography--display-3" style={{fontWeight: '200'}}>Try Recomea now!</p>
-                <div className="mdl-layout-spacer" />
+            <div
+              className="logo-font logo-slogan mdl-typography--text-center">
+              Try Recomea now!
             </div>
-            <div className="mdl-grid" style={{padding: '10px'}}>
-                <div className="mdl-layout-spacer" />
-                <p className="mdl-typography--body-2" style={{fontWeight: '400'}}>Recomea gives you an opportunity to share something with the whole world.</p>
-                <div className="mdl-layout-spacer" />
+            <div
+              className="logo-font logo-sub-slogan mdl-typography--text-center">
+              Recomea gives you an opportunity to share something with the whole world.
             </div>
-            <div className="mdl-grid" style={{padding: '20px'}}>
+            <div className="mdl-grid">
                 <div className="mdl-layout-spacer" />
                 <FacebookButton />
                 <div className="mdl-layout-spacer" />
