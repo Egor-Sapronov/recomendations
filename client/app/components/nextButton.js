5415c47f9cb931a97b5ac05e3724bdafcce0ed44
Egor <sapronov.egor@gmail.com>
2015-11-22 16:22:54 +0600

    fox

--- client/app/components/nextButton.js
+++ client/app/components/nextButton.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <div style={{padding: '40px'}}>
-            <div className="mdl-grid">
-                <div className="mdl-layout-spacer" />
-                <p className="mdl-typography--display-2" style={{fontWeight: '200'}}>No recommendation ;(</p>
-                <div className="mdl-layout-spacer" />
+        <div>
+            <div className="logo-font logo-slogan mdl-typography--text-center">
+              No recommendation ;(
             </div>
-            <div className="mdl-grid">
+            <div className="mdl-grid" style={{paddingTop: '20px'}}>
                 <div className="mdl-layout-spacer" />
                 <button onClick={ props.onClick } className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored">
                     next
